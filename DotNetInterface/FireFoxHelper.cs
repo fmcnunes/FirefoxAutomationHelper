240381de961195fe23795bc0c7f3dbcbdc73cc44
x952429 <fernando.nunes@millenniumbcp.pt>
2016-11-13 17:51:51 +0000

    .Net interface included. Firefox addon code improved.

---
+++ DotNetInterface/FireFoxHelper.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace FireFoxHelperLib
+{
+    public class BrowserCounters
+    {
+
+        public int Counter { get; set; }
+        public int CounterTotal { get; set; }
+        public int LastReset { get; set; }
+        public int LastEvent { get; set; }
+        public string Version { get; set; }
+        public string DomainList { get; set; }
+
+        public BrowserCounters ()
+        {
+            Counter = 0;
+            CounterTotal = 0;
+            LastReset = 0;
+            LastEvent = 0;
+            Version = "?";
+            DomainList = "?";
+        }
+    }
+
+    public class FireFoxHelper
+    {
+        private String FirefoxUrl = @"http://localhost:8080";
+        private static object oLock = new object();
+
+        public FireFoxHelper(String ffUrl)
+        {
+            FirefoxUrl = ffUrl;
+        }
+
+        public FireFoxHelper()
+        {
+        }
+
+        public String SetUrlfilter(String filter)
+        {
+            BrowserCounters browserCounters = new BrowserCounters();
+
+            try
+            {
+                browserCounters = GetData(@"/domainlist=" + filter);
+                browserCounters = GetData(@"/domainlist=" + filter);
+                if (browserCounters.DomainList.Equals(filter))
+                    return "OK";
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return "FAIL";
+        }
+
+        public String ResetBrowserCounters(out BrowserCounters BrowserCounters)
+        {
+            BrowserCounters = new BrowserCounters();
+
+            try
+            {
+                GetData(@"/reset");
+                BrowserCounters = GetData(@"/reset");
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return "OK";
+        }
+
+        public BrowserCounters GetData()
+        {
+            return GetData("");
+        }
+
+        public BrowserCounters GetData(String url)
+        {
+            BrowserCounters browserCounters = new BrowserCounters();
+            lock (oLock)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    try
+                    {
+                        String json = GET(FirefoxUrl + url);
+
+                        if (json.Contains("{"))
+                        {
+                            browserCounters = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<BrowserCounters>(json);
+                            return browserCounters;
+                        }
+                    }
+                    catch (Exception) { }
+                    Thread.Sleep(200);
+                }
+            }
+            return browserCounters;
+        }
+
+
+        private string GET(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Timeout = 450000;
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+
+                try
+                {
+                    using (Stream responseStream = errorResponse.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                        String errorText = reader.ReadToEnd();
+                        // log errorText
+                    }
+                }
+                catch (Exception) { }
+                return "";
+            }
+        }
+
+
+    }
+}
--- Source/index.js
+++ Source/index.js
@@ -1,192 +1,224 @@
 var self = require("sdk/self");
-const {Cc, Cu, Ci} = require("chrome");
-Cu.import("resource://gre/modules/XPCOMUtils.jsm");1
+const {
+    Cc,
+    Cu,
+    Ci
+} = require("chrome");
+Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Console.jsm");
+
+/************************************************************************
+ *
+ * Global variables
+ *
+ *************************************************************************/
+
+var gWebServerListener;
+
+var serverSocket;
 
 var gCounter = 0;
 
+var gCounterTotal = 0;
+
 var gLastRequest = new Date();
+
+var gLastReset = new Date();
 
 var WebSiteDomainList = "mozilla.org,google.pt"
 
-console.log("Extension executed");
-
-StartServer();
-
-console.log("Adding Listener");
-
-
-/************************************************************************/
-
-var httpRequestObserver =
+/************************************************************************
+ *
+ * Logger
+ *
+ *************************************************************************/
+function LogMessage(msg)
 {
-  observe: function(subject, topic, data)
-  {
-	subject.QueryInterface(Ci.nsIHttpChannel);
-    url = subject.URI.spec;
-    
-    var domainList = WebSiteDomainList.split(',');
-    for (var i = 0; i < domainList.length; i++)
-    {
-        if (url.indexOf(domainList[i]) !== -1)
-        {
-            if (topic == "http-on-modify-request")
-            {
-                gCounter++;
-                gLastRequest = new Date();
-                console.log("Request (" + gCounter + "): " + topic + " -> " + url);
+    console.error(msg);
+}
+
+/************************************************************************
+ *
+ * Load and Unload functions
+ *
+ *************************************************************************/
+
+exports.main = function() {
+    LogMessage("FirefoxAutimationHelper loading");
+
+    webServer.onLoad();
+
+    httpRequestObserver.register();
+
+    LogMessage("FirefoxAutimationHelper up and running.");
+}
+
+exports.onUnload = function() {
+    LogMessage("FirefoxAutimationHelper unloading");
+
+    httpRequestObserver.unregister();
+
+    webServer.onUnLoad();
+
+    LogMessage("FirefoxAutimationHelper unloaded.");
+}
+
+/************************************************************************
+ *
+ * HTTP Request Observers 
+ *
+ ************************************************************************/
+
+var httpRequestObserver = {
+    observe: function(subject, topic, data) {
+        subject.QueryInterface(Ci.nsIHttpChannel);
+        url = subject.URI.spec;
+
+        var domainList = WebSiteDomainList.split(',');
+        for (var i = 0; i < domainList.length; i++) {
+            if (url.indexOf(domainList[i]) !== -1) {
+                if (topic == "http-on-modify-request") {
+                    gCounter++;
+                    gCounterTotal++;
+                    gLastRequest = new Date();
+                    LogMessage("Request (" + gCounter + "): " + topic + " -> " + url);
+                } else {
+                    gCounter--;
+                    gLastRequest = new Date();
+                    LogMessage("Resposta (" + gCounter + "): " + topic + " -> " + url);
+                }
             }
-            else
-            {
-                gCounter--;
-                gLastRequest = new Date();
-                console.log("Resposta (" + gCounter + "): " + topic + " -> " + url);
-            }
         }
-    }
-  },
-
-  get observerService() {
-    return Cc["@mozilla.org/observer-service;1"]
-                     .getService(Ci.nsIObserverService);
-  },
-
-  register: function()
-  {
-    this.observerService.addObserver( this, "http-on-modify-request", false);
-	this.observerService.addObserver( this, "http-on-examine-response", false);
-    this.observerService.addObserver( this, 'http-on-examine-cached-response', false );
-    this.observerService.addObserver( this, 'http-on-examine-merged-response', false );
-  },
-
-  unregister: function()
-  {
-    this.observerService.removeObserver(this, "http-on-modify-request");
-  }
-};
-
-httpRequestObserver.register();
-
-/*************************************************************************************************************/
-
-
-function StartServer()
-{
-  console.log("Starting WebServer....");
-  var listener =
-  {
-    onSocketAccepted : function(socket, transport)
-    {
-	  console.log("> Request received.");
-      try {
-	    var endTime = new Date();
-		var elapsed = 0;
-		
-		elapsed = endTime-gLastRequest;
-
-        var stream = transport.openOutputStream(0,0,0);
-		var streamInput  = transport.openInputStream(0, 0, 0);
-        
-        var sin = Cc["@mozilla.org/scriptableinputstream;1"].createInstance(Ci.nsIScriptableInputStream);
-
-        sin.init(streamInput);
-
-	
-		var resp= "{ \"Counter\": " + gCounter + ", \"Elapsed\": " + elapsed + ", \"Version\": 1, \"DomainList\": \"" + WebSiteDomainList + "\"}";
-		var outputString = "HTTP/1.1 200 OK\r\nContent-Length: "+resp.length+"\r\nContent-Type: text/html\r\n\r\n"+resp;
-		
-        console.log("> Send response: " + outputString);
-		
-        stream.write(outputString,outputString.length);
-        
-        var request = '';
-
-        while (sin.available())
-        {
-          request = request + sin.read(512);
-        }
-        console.log('Received: ' + request);
-        
-        var re = new RegExp(/Get\s+\/domainlist=(\S+)\s+HTTP/igm);
-        var match = re.exec(request);
-        
-        console.log("Testing received text");
-
-        if (match !== null)
-        {
-          console.log("Match is not null");
-          gCounter=0;
-          gLastRequest = new Date();
-          WebSiteDomainList=match[1];
-          console.log("Setting WebSiteDomainList to " + WebSiteDomainList);
-        }
-        else
-            console.log("Match is null");
-        
-        var re = new RegExp(/Get\s+\/reset\s+HTTP/igm);
-        var match = re.exec(request);
-        
-        console.log("Testing received text");
-
-        if (match !== null)
-        {
-          console.log("Reset counters command received.");
-          gCounter=0;
-          gLastRequest = new Date();
-        }
-        else
-            console.log("Match is null");
-        
-        stream.close();
-		streamInput.close();
-      } catch(ex2){ console.log("> Error processing Webserver http request" + ex2); dump("::"+ex2); }
     },
 
-    onStopListening : function(socket, status){}
-  };
+    get observerService() {
+        return Cc["@mozilla.org/observer-service;1"]
+            .getService(Ci.nsIObserverService);
+    },
 
-  console.log("Starting WebServer on port 8080");
-  try {
-	const {Cc, Ci} = require("chrome");
-    var serverSocket = Cc["@mozilla.org/network/server-socket;1"].createInstance(Ci.nsIServerSocket);
+    register: function() {
+        this.observerService.addObserver(this, "http-on-modify-request", false);
+        this.observerService.addObserver(this, "http-on-examine-response", false);
+        this.observerService.addObserver(this, 'http-on-examine-cached-response', false);
+    },
 
-
-    serverSocket.init(8080,false,-1);
-    serverSocket.asyncListen(listener);
-  }
-  catch(ex)
-  { 
-    console.log("Error in WebServer: " + ex);
-	dump(ex);
-  }
-  console.log("WebServer is running.");
-}
-
-
-
-var AutomationHelper = {
-  onLoad: function() {
-    
-	console.log("Starting WebServer");
-	// Inicia o WebServer
-	StartServer();
-	
-    this.initialized = true;
-
-  }
+    unregister: function() {
+        this.observerService.removeObserver(this, "http-on-modify-request");
+        this.observerService.removeObserver(this, "http-on-examine-response");
+        this.observerService.removeObserver(this, "http-on-examine-cached-response");
+    }
 };
 
-function getMainWindow() {
-	const {Cc, Ci} = require("chrome");
-  var windowManager = Cc['@mozilla.org/appshell/window-mediator;1'].getService();
-  var windowManagerInterface = windowManager.QueryInterface(Ci.nsIWindowMediator);
-  var eb = windowManagerInterface.getEnumerator("navigator:browser");
-  if (eb.hasMoreElements()) {
-    return eb.getNext().QueryInterface(Ci.nsIDOMWindow);
-  }
-  return null;
-}
+/************************************************************************
+ *
+ * WebServer 
+ *
+ ************************************************************************/
 
-function getBrowser() {
-  return getMainWindow().getBrowser();
-}
+var webServer = {
+    onLoad: function() {
 
+        LogMessage("Listening for HTTP command requests");
+        gWebServerListener = {
+            onSocketAccepted: function(socket, transport) {
+                LogMessage("> Request received.");
+                try {
+                    var endTime = new Date();
+                    var lastEvent = 0;
+                    var LastReset = 0;
+
+                    lastEvent = endTime - gLastRequest;
+                    LastReset = endTime - gLastReset;
+
+                    var stream = transport.openOutputStream(0, 0, 0);
+                    var streamInput = transport.openInputStream(0, 0, 0);
+
+                    var sin = Cc["@mozilla.org/scriptableinputstream;1"].createInstance(Ci.nsIScriptableInputStream);
+
+                    sin.init(streamInput);
+
+
+                    var resp = "{ \"Counter\": " + gCounter + ", \"CounterTotal\": " + gCounterTotal + ",\"LastEvent\": " + lastEvent + ",\"LastReset\": " + LastReset + ", \"Version\": 1, \"DomainList\": \"" + WebSiteDomainList + "\"}";
+                    var outputString = "HTTP/1.1 200 OK\r\nContent-Length: " + resp.length + "\r\nContent-Type: text/html\r\n\r\n" + resp;
+
+                    LogMessage("> Send response: " + outputString);
+
+                    stream.write(outputString, outputString.length);
+
+                    var request = '';
+
+                    while (sin.available()) {
+                        request = request + sin.read(512);
+                    }
+                    LogMessage('Received: ' + request);
+
+                    var re = new RegExp(/Get\s+\/domainlist=(\S+)\s+HTTP/igm);
+                    var match = re.exec(request);
+
+                    LogMessage("Testing received text");
+
+                    if (match !== null) {
+                        LogMessage("Match is not null");
+                        gCounter = 0;
+                        gCounterTotal = 0;
+                        gLastRequest = new Date();
+                        WebSiteDomainList = match[1];
+                        LogMessage("Setting WebSiteDomainList to " + WebSiteDomainList);
+                    } else
+                        LogMessage("Match is null");
+
+                    var re = new RegExp(/Get\s+\/reset\s+HTTP/igm);
+                    var match = re.exec(request);
+
+                    LogMessage("Testing received text");
+
+                    if (match !== null) {
+                        LogMessage("Reset counters command received.");
+                        gCounter = 0;
+                        gCounterTotal = 0;
+                        gLastRequest = new Date();
+                        gLastReset = new Date();
+                    } else
+                        LogMessage("Match is null");
+
+                    stream.close();
+                    streamInput.close();
+                } catch (ex2) {
+                    LogMessage("> Error processing Webserver http request" + ex2);
+                    dump("::" + ex2);
+                }
+            },
+
+            onStopListening: function(socket, status) {}
+        };
+
+
+        // Inicia o WebServer
+        LogMessage("Starting WebServer on port 8080");
+        try {
+            const {
+                Cc,
+                Ci
+            } = require("chrome");
+            serverSocket = Cc["@mozilla.org/network/server-socket;1"].createInstance(Ci.nsIServerSocket);
+
+
+            serverSocket.init(8080, false, -1);
+            serverSocket.asyncListen(gWebServerListener);
+        } catch (ex) {
+            LogMessage("Error in WebServer: " + ex);
+            dump(ex);
+        }
+        LogMessage("WebServer is running.");
+
+        this.initialized = true;
+
+    },
+
+    onUnLoad: function() {
+
+        LogMessage("UnLoading WebServer");
+        
+        serverSocket.close();
+    }
+};
